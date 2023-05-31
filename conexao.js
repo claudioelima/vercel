96346dfa9b368d25016aace251aab1f73765eaf3
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2023-05-31 10:34:23 -0300

    Add files via upload

---
+++ conexao.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+// Configurações de conexão com o banco de dados MySQL
+const connection = mysql.createConnection({
+  host: '0.0.0.0',
+  user: '',
+  password: '',
+  database: ''
+});
+
+// Conecta-se ao banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro de conexão:', err);
+  } else {
+    console.log('Conexão bem-sucedida ao banco de dados!');
+  }
+});
+
+// Rota para realizar o SELECT e exibir os resultados
+app.get('/dados', (req, res) => {
+  connection.query('SELECT * FROM cliente', (err, results) => {
+    if (err) {
+      console.error('Erro na consulta:', err);
+      res.status(500).send('Erro ao consultar o banco de dados.');
+    } else {
+      console.log('Resultados:', results);
+      res.send(results);
+    }
+  });
+});
+
+// Inicia o servidor na porta desejada
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000');
+});
+
+  
